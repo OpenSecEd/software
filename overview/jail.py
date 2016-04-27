e119c468c231452249d705ff4a7239085238ef78
Daniel Bosk <dbosk@kth.se>
2016-04-27 20:01:04 +0200

    Adds Python examples (#1), WIP on runnable examples (#4)
    Fixes #1, still some way to go on #4.

---
+++ overview/jail.py
@@ -0,0 +1,16 @@
+#!/bin/env python3
+import sys, os
+
+JAIL_PATH = os.environ["HOME"]
+
+def jailed_open(filename):
+    return open(JAIL_PATH + "/" + filename)
+
+def main(argv):
+    f = jailed_open(argv[1])
+
+    for line in f.readlines():
+        print(line)
+
+if __name__ == "__main__":
+    main(sys.argv)
