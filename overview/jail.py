2239d69be5360927beffe6063e9900f4c0ddea18
Daniel Bosk <dbosk@kth.se>
2017-01-08 12:01:17 +0100

    overview: Adds verbatim environment in jail.py output

--- overview/jail.py
+++ overview/jail.py
@@ -9,8 +9,10 @@ def jailed_open(filename):
 def main(argv):
     f = jailed_open(argv[1])
 
+    print("\\begin{verbatim}")
     for line in f.readlines():
-        print(line)
+        print(line.strip())
+    print("\\end{verbatim}\n")
 
 if __name__ == "__main__":
     main(sys.argv)
